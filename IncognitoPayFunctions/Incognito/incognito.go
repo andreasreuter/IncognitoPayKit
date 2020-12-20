de94968f9f2728ea89c4952c81e2a924e29d7d69
Andreas Reuter <andreasreuter@yahoo.com>
2020-12-20 12:18:49 +0100

    Update docs

--- IncognitoPayFunctions/Incognito/incognito.go
+++ IncognitoPayFunctions/Incognito/incognito.go
@@ -6,6 +6,10 @@ import (
 	"github.com/incognitochain/go-incognito-sdk/incognitoclient"
 )
 
+//
+// IncognitoBlockchain creates an instance of Incognito client
+// to connect with Incognito Blockchain.
+//
 func IncognitoBlockchain() *incognitoclient.Blockchain {
 	client := &http.Client{}
 	incognitoBlockchain := incognitoclient.NewBlockchain(
